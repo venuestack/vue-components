ea298cfffcd872152ec9b0332755644aa2ac0547
Ryan Clarke <ryan.w.clarke@outlook.com>
2023-12-04 10:14:08 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+  import { Button } from 'vue-components'
+</script>
+
+<template>
+    <Button text="Hello World" />
+</template>
---
+++ src/components/Button.vue
@@ -0,0 +1,12 @@
+
+<script setup lang="ts">
+  import { defineProps } from 'vue';
+
+  defineProps<{
+    text: string;
+  }>();
+</script>
+
+<template>
+  <button class="px-6 py-2 bg-slate-950 text-white hover:bg-slate-800 rounded-md">{{ text }}</button>
+</template>
